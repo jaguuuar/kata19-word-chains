717084541ef52d40bc356c848e0eef4f64ed2762
Kamil Kmieć <sirkamson@gmail.com>
2018-08-19 19:33:23 +0200

    Implement ChainService methods

--- src/main/java/com/kata/chain/service/ChainServiceImpl.java
+++ src/main/java/com/kata/chain/service/ChainServiceImpl.java
@@ -1,6 +1,11 @@
 package com.kata.chain.service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Queue;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,8 +23,41 @@ public class ChainServiceImpl implements ChainService {
 
     @Override
     public List<String> findChain(String startWord, String endWord) {
-        return null;
+        Map<String, String> wordTree = getWordTree(startWord, endWord);
+
+        if (!wordTree.containsKey(endWord)) {
+            return new ArrayList<>();
+        }
+
+        List<String> chain = new ArrayList<>();
+        String word = endWord;
+        while (!word.equals(startWord)) {
+            chain.add(0, word);
+            word = wordTree.get(word);
+        }
+        chain.add(0, startWord);
+
+        return chain;
     }
 
+    private Map<String, String> getWordTree(String startWord, String endWord) {
+        Map<String, String> wordTree = new HashMap<>();
+        List<String> worldList = filterService.getWords(startWord.length());
+        Queue<String> wordQueue = new LinkedList<>();
+        wordQueue.add(startWord);
 
+        String parentWord = "";
+        while (!wordQueue.isEmpty() && !parentWord.equals(endWord)) {
+            parentWord = wordQueue.remove();
+            List<String> childrenWord = filterService.getChildrenWord(parentWord, worldList);
+            for (String child : childrenWord) {
+                if (!wordTree.containsKey(child)) {
+                    wordTree.put(child, parentWord);
+                    wordQueue.add(child);
+                }
+            }
+        }
+
+        return wordTree;
+    }
 }
